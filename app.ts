c0b026d4d1f67268c1cb192e6689426175084340
Martin Markov (Personal) <martin.markov1980@gmail.com>
2025-11-17 15:37:06 +0000

    Loops and conrol flow.

--- app.ts
+++ app.ts
@@ -1,11 +1,40 @@
-let names: string[] = ['Martin', 'Mitko'];
+console.log('For loop results:');
 
-let namesG: Array<string> = ['Martin', 'Mitko'];
 
-let arrMixValues: (string | number)[] = [15, 'Volvo'];
-
-for (const value of arrMixValues) {
-    console.log(value);
+for (let index = 1; index < 10; index++) {
+    if (index % 2 === 0) {
+        console.log(`Number ${index} is even`);
+    }   else {
+        console.log((`Number ${index} is odd`));   
+    }
 }
 
+console.log('While loop results:');
 
+let startNumber: number = 1;
+
+while (startNumber < 10) {
+    if (startNumber % 2 === 0) {
+        console.log(`Number ${startNumber} is even`);
+    }   else {
+        console.log((`Number ${startNumber} is odd`));   
+    }
+
+    startNumber++
+}
+
+console.log('Switch results:');
+
+let fruit: string = 'Orange';
+
+switch (fruit) {
+    case 'Orange':
+        console.log('The fruit is tropical');
+        break;
+    case 'Apple':
+        console.log('The fruit is not tropical');
+        break;
+    default:
+        console.log('Not known fruit');
+        break;
+}
--- js/app.js
+++ js/app.js
@@ -1,7 +1,34 @@
 "use strict";
-let names = ['Martin', 'Mitko'];
-let namesG = ['Martin', 'Mitko'];
-let arrMixValues = [15, 'Volvo'];
-for (const value of arrMixValues) {
-    console.log(value);
+console.log('For loop results:');
+for (let index = 1; index < 10; index++) {
+    if (index % 2 === 0) {
+        console.log(`Number ${index} is even`);
+    }
+    else {
+        console.log((`Number ${index} is odd`));
+    }
+}
+console.log('While loop results:');
+let startNumber = 1;
+while (startNumber < 10) {
+    if (startNumber % 2 === 0) {
+        console.log(`Number ${startNumber} is even`);
+    }
+    else {
+        console.log((`Number ${startNumber} is odd`));
+    }
+    startNumber++;
+}
+console.log('Switch results:');
+let fruit = 'Nut';
+switch (fruit) {
+    case 'Orange':
+        console.log('The fruit is tropical');
+        break;
+    case 'Apple':
+        console.log('The fruit is not tropical');
+        break;
+    default:
+        console.log('Not known fruit');
+        break;
 }
