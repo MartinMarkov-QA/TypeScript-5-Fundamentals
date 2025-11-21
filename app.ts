f809a6888c94c78db4116802de6bc0a64798b11f
Martin Markov (Personal) <martin.markov1980@gmail.com>
2025-11-21 08:14:18 +0000

    Function Overloads.

--- app.ts
+++ app.ts
@@ -1,4 +1,40 @@
-// Arrow function
-const greeting = (message: string) => {console.log(message)};
+// Function Overload 
+const movies = [
+    { movieTitle: 'One', author: 'Jeff', streaming: false },
+    { movieTitle: 'Two', author: 'Alex', streaming: true },
+    { movieTitle: 'Three', author: 'Jeff', streaming: true },
+    { movieTitle: 'Four', author: 'Alex', streaming: false },
+    { movieTitle: 'Five', author: 'Jeff', streaming: true },
+];
 
-greeting('Hello!!!');
+// Function signatures
+function getMoviesTitles(author: string): string[];
+function getMoviesTitles(author: string, streaming: boolean): string[];
+
+// Function implementation
+function getMoviesTitles(author: string, streaming?: boolean) {
+    let movieTitles: string[] = [];
+
+    if (streaming !== undefined) {
+        for (const movie of movies) {
+            if (movie.author === author && streaming === movie.streaming) {
+                movieTitles.push(movie.movieTitle)
+            }
+        }
+
+    } else {
+        for (const movie of movies) {
+            if (author === movie.author) {
+                movieTitles.push(movie.movieTitle);
+            }
+        }
+    }
+
+    return movieTitles;
+}
+
+let results: string[] = getMoviesTitles('Jeff', true);
+
+for (const result of results) {
+    console.log(result);
+}
--- js/app.js
+++ js/app.js
@@ -1,4 +1,32 @@
 "use strict";
-// Arrow function
-const greeting = (message) => { console.log(message); };
-greeting('Hello!!!');
+// Function Overload 
+const movies = [
+    { movieTitle: 'One', author: 'Jeff', streaming: false },
+    { movieTitle: 'Two', author: 'Alex', streaming: true },
+    { movieTitle: 'Three', author: 'Jeff', streaming: true },
+    { movieTitle: 'Four', author: 'Alex', streaming: false },
+    { movieTitle: 'Five', author: 'Jeff', streaming: true },
+];
+// Function implementation
+function getMoviesTitles(author, streaming) {
+    let movieTitles = [];
+    if (streaming !== undefined) {
+        for (const movie of movies) {
+            if (movie.author === author && streaming === movie.streaming) {
+                movieTitles.push(movie.movieTitle);
+            }
+        }
+    }
+    else {
+        for (const movie of movies) {
+            if (author === movie.author) {
+                movieTitles.push(movie.movieTitle);
+            }
+        }
+    }
+    return movieTitles;
+}
+let results = getMoviesTitles('Jeff', true);
+for (const result of results) {
+    console.log(result);
+}
