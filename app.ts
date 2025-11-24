eeaf4970811be8d9ba94eeed39a80652c9fc6165
Martin Markov (Personal) <martin.markov1980@gmail.com>
2025-11-24 22:02:05 +0000

    TypeScript Classes.

--- app.ts
+++ app.ts
@@ -1,32 +1,21 @@
-// Interface extending
+// Classes 
 
-interface Machine {
-    factoryNumber: number;
+class Car {
+    constructor(public carName: string, public carSpeed: number) {};
+
+    static classType: string = 'Car';
+
+    public drive(): void {
+        console.log(`I am driving ${this.carName} with ${this.carSpeed} km/h.`);
+    }
 }
 
-interface CarType extends Machine {
-    carType: string
-}
+const mercedes = new Car('Mercedes', 187);
+mercedes.drive();
 
-interface SportsCar extends CarType {
-    maxSpeed: number;
-    drive: (speed: number) => number; 
-}
+console.log(Car.classType);
 
-let ferrari: SportsCar = {
-    factoryNumber: 17,
-    carType: 'Sport Car',
-    maxSpeed: 189,
-    drive(speed) {
-        return speed;
-    },
-}
 
-console.log(
-    `The car factory number is ${ferrari.factoryNumber}. 
-    The car type is ${ferrari.carType}. 
-    The car max speed is ${ferrari.maxSpeed}.
-    The car is currently driving with ${ferrari.drive(117)} km per hour`
-);
+
 
 
--- js/app.js
+++ js/app.js
@@ -1,14 +1,16 @@
 "use strict";
-// Interface extending
-let ferrari = {
-    factoryNumber: 17,
-    carType: 'Sport Car',
-    maxSpeed: 189,
-    drive(speed) {
-        return speed;
-    },
-};
-console.log(`The car factory number is ${ferrari.factoryNumber}. 
-    The car type is ${ferrari.carType}. 
-    The car max speed is ${ferrari.maxSpeed}.
-    The car is currently driving with ${ferrari.drive(117)} km per hour`);
+// Classes 
+class Car {
+    constructor(carName, carSpeed) {
+        this.carName = carName;
+        this.carSpeed = carSpeed;
+    }
+    ;
+    drive() {
+        console.log(`I am driving ${this.carName} with ${this.carSpeed} km/h.`);
+    }
+}
+Car.classType = 'Car';
+const mercedes = new Car('Mercedes', 187);
+mercedes.drive();
+console.log(Car.classType);
