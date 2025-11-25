981229832f1d16fa9ea36d0b48aed73356a8cb25
Martin Markov (Personal) <martin.markov1980@gmail.com>
2025-11-25 07:43:36 +0000

    TypeScript Classes.

--- app.ts
+++ app.ts
@@ -1,19 +1,31 @@
 // Classes 
 
-class Car {
-    constructor(public carName: string, public carSpeed: number) {};
+class Human {
 
-    static classType: string = 'Car';
+    private _age: number = 0;
 
-    public drive(): void {
-        console.log(`I am driving ${this.carName} with ${this.carSpeed} km/h.`);
+    get age(): number {
+        return this._age;
+    }
+
+    set age(age) {
+        this._age = age;
+    }
+
+    constructor(public gender: string, public name: string) {
+        console.log(`The human has been created.`);
+    }
+
+    public walk(): void {
+        console.log(`The man is walking.`);
     }
 }
 
-const mercedes = new Car('Mercedes', 187);
-mercedes.drive();
+let martin: Human = new Human('Man', 'Martin');
+martin.age = 45;
+console.log(martin.age);
+martin.walk();
 
-console.log(Car.classType);
 
 
 
--- js/app.js
+++ js/app.js
@@ -1,16 +1,23 @@
 "use strict";
 // Classes 
-class Car {
-    constructor(carName, carSpeed) {
-        this.carName = carName;
-        this.carSpeed = carSpeed;
+class Human {
+    get age() {
+        return this._age;
     }
-    ;
-    drive() {
-        console.log(`I am driving ${this.carName} with ${this.carSpeed} km/h.`);
+    set age(age) {
+        this._age = age;
+    }
+    constructor(gender, name) {
+        this.gender = gender;
+        this.name = name;
+        this._age = 0;
+        console.log(`The human has been created.`);
+    }
+    walk() {
+        console.log(`The man is walking.`);
     }
 }
-Car.classType = 'Car';
-const mercedes = new Car('Mercedes', 187);
-mercedes.drive();
-console.log(Car.classType);
+let martin = new Human('Man', 'Martin');
+martin.age = 45;
+console.log(martin.age);
+martin.walk();
