5de9edbf6d81ec1ff5d451d0906458e35c979154
Martin Markov (Personal) <martin.markov1980@gmail.com>
2025-11-25 08:27:43 +0000

    TypeScript extending Classes.

--- app.ts
+++ app.ts
@@ -1,36 +1,29 @@
-// Classes 
-
 class Human {
-
-    private _age: number = 0;
-
-    get age(): number {
-        return this._age;
+    constructor(humanName: string) {
+        console.log(`I am Human ${humanName} called from the parent constructor.`);
     }
 
-    set age(age) {
-        this._age = age;
+    protected walk(): void {
+        console.log('The Human is walking');
+    } 
+}
+
+class Man extends Human {
+    constructor(private manName: string) {
+        super(manName);
     }
 
-    constructor(public gender: string, public name: string) {
-        console.log(`The human has been created.`);
-    }
-
-    public walk(): void {
-        console.log(`The man is walking.`);
-    }
-
-    static whatTypeAmI(): void {
-        console.log('I am Human type.');
-        
+    public walk(): number {
+        super.walk();
+        return 45;
     }
 }
 
-let martin: Human = new Human('Man', 'Martin');
-martin.age = 45;
-console.log(martin.age);
-martin.walk();
-Human.whatTypeAmI();
+let martin = new Man('Martin');
+console.log(martin.walk());
+
+
+
 
 
 
--- js/app.js
+++ js/app.js
@@ -1,27 +1,21 @@
 "use strict";
-// Classes 
 class Human {
-    get age() {
-        return this._age;
-    }
-    set age(age) {
-        this._age = age;
-    }
-    constructor(gender, name) {
-        this.gender = gender;
-        this.name = name;
-        this._age = 0;
-        console.log(`The human has been created.`);
+    constructor(humanName) {
+        console.log(`I am Human ${humanName} called from the parent constructor.`);
     }
     walk() {
-        console.log(`The man is walking.`);
-    }
-    static whatTypeAmI() {
-        console.log('I am Human type.');
+        console.log('The Human is walking');
     }
 }
-let martin = new Human('Man', 'Martin');
-martin.age = 45;
-console.log(martin.age);
-martin.walk();
-Human.whatTypeAmI();
+class Man extends Human {
+    constructor(manName) {
+        super(manName);
+        this.manName = manName;
+    }
+    walk() {
+        super.walk();
+        return 45;
+    }
+}
+let martin = new Man('Martin');
+console.log(martin.walk());
