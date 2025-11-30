33e44b0b3a7da311ebfc27174a130925adbf22ed
Martin Markov (Personal) <martin.markov1980@gmail.com>
2025-11-30 12:33:39 +0000

    Promise try catch.

--- app.ts
+++ app.ts
@@ -1,30 +1,43 @@
-// Promises
+let data: number[] = [];
 
-let apiData: string[] = ["a", "c", "b"];
+function getData(data: number[]): Promise<number[]> {
+    return new Promise<number[]>((resolve, reject) => {
+        setTimeout(() => {
+            if (data.length > 0) {
+            resolve(data);
+        } else {
+            reject('No data at this time!');
+        }
+        }, 2000)
+    })
+}
 
-function resProm(data: string[]): Promise<string[]> {
-  return new Promise<string[]>((resolve, reject) => {
-    setTimeout(() => {
-      if (data.length > 0) {
-        resolve(data);
-      } else {
-        reject("No data found");
-      }
-    }, 2000);
-  });
-};
+console.log("Start One!");
 
-
-console.log('Start!');
-
-resProm(apiData)
-.then((data) => {
-    console.log(`The data is ${data} !`);
+getData(data)
+.then((d) => {
+    console.log(`Here is your data: ${data} one!`);
 })
-.catch((data) => {
-    console.log(data);
+.catch((d) => {
+    console.log(d);
+    
 })
 
-console.log('End!');
+console.log("Finish One!");
 
+console.log("Start Two!");
+
+async function rData(data: number[]): Promise<void> {
+    try {
+        let res = await getData(data);
+        console.log(`Here is your data: ${res} two!`);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+rData(data);
+
+console.log("Finish Teo!");
+    
 
--- js/app.js
+++ js/app.js
@@ -1,25 +1,46 @@
 "use strict";
-// Promises
-let apiData = ["a", "c", "b"];
-function resProm(data) {
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+let data = [];
+function getData(data) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             if (data.length > 0) {
                 resolve(data);
             }
             else {
-                reject("No data found");
+                reject('No data at this time!');
             }
         }, 2000);
     });
 }
-;
-console.log('Start!');
-resProm(apiData)
-    .then((data) => {
-    console.log(`The data is ${data} !`);
+console.log("Start One!");
+getData(data)
+    .then((d) => {
+    console.log(`Here is your data: ${data} one!`);
 })
-    .catch((data) => {
-    console.log(data);
+    .catch((d) => {
+    console.log(d);
 });
-console.log('End!');
+console.log("Finish One!");
+console.log("Start Two!");
+function rData(data) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            let res = yield getData(data);
+            console.log(`Here is your data: ${res} two!`);
+        }
+        catch (err) {
+            console.log(err);
+        }
+    });
+}
+rData(data);
+console.log("Finish Teo!");
